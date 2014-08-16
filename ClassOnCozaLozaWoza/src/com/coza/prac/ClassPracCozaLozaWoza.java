faf66d7ca330ba462e9877f2e25f9be581d725bf
MD JAMIL ANOWARI <jamil.anowari19@gmail.com>
2014-08-16 22:27:49 +0600

    Today Class on CozaLozaWoza

---
+++ ClassOnCozaLozaWoza/src/com/coza/prac/ClassPracCozaLozaWoza.java
@@ -0,0 +1,37 @@
+package com.coza.prac;
+
+public class ClassPracCozaLozaWoza {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+			      int lowerbound = 1;
+			      int upperbound = 110;
+			      for (int number = lowerbound; number <= upperbound; ++number) {
+			    	  
+			    	  if (number/3 == 0) {
+			    	  }
+			    	  System.out.println("Coza");
+			    	  
+			    	  if (number/5 == 0) {
+			    	  }
+			    	  System.out.println("Loza");
+			    	  
+			    	  if (number/7 == 0) {
+			    	  }
+			    	  System.out.println("Woza");
+			    	  
+			    	  if (number/3 !=0 && number/5 !=0 && number/7 !=0) {
+			    	System.out.println(number);
+			    	
+			    	
+			    	  
+			    	  }			    	  
+			    	  
+			    	  }
+			    	  
+			      } 	  
+			    	  
+			         			      }
+			   
+
